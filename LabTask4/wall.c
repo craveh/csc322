b4963ae69a6680bad692d0bd16845ec4963100a4
craveh <61929297+craveh@users.noreply.github.com>
2021-10-01 15:22:59 -0400

    Update wall.c

--- LabTask4/wall.c
+++ LabTask4/wall.c
@@ -1,1 +1,43 @@
-//---------------------------
+//-----------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//----------------------------------------
+float WallAreaCalc(float Height, float Width){
+  float Area = Height * Width;
+  printf("Wall area: %.2f sq feet \n", Area);
+  return Area;
+}
+
+float PaintNeededCalc(float WallArea){
+  float Paint = WallArea/ 350;
+  printf("Gallons of paint needed: %.2f gallons\n", Paint);
+
+  return Paint;
+}
+
+void CalcPaintCans(float Gallons){
+
+  int NumCans = ceil(Gallons);
+  printf("Cans of paint needed: %d can(s)\n\n", NumCans);
+}
+
+int main(void){
+  float Height, Width;
+  //get all the info needed
+  printf("Enter the height of the wall in feet: ");
+  scanf("%f", &Height);
+
+  printf("Enter the width of the wall in feet : ");
+  scanf("%f", &Width);
+
+  printf("\n");
+
+  //do calcs
+  float Area = WallAreaCalc(Height, Width);
+  float Paint = PaintNeededCalc(Area);
+  CalcPaintCans(Paint);
+}
+
+//--------------------------------------------------
