e95106092bae7787bcaa7021151fee512ad3bd42
craveh <61929297+craveh@users.noreply.github.com>
2021-12-09 13:37:42 -0500

    Add files via upload

---
+++ Final Practice Code/SecondProcess.c
@@ -0,0 +1,24 @@
+//-----------------------------
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+//-----------------------------
+int main(){
+    int ChildPID;
+
+    if((ChildPID = fork()) == -1){
+        perror("Could not fork and run the child process");
+        exit(1);
+    }
+
+    if(ChildPID == 0){
+        printf("The childs PID is %d and the user name is %s", getpid(), getuid());
+    }else{
+        sleep(5);
+        printf("The parents PID is %d and the user name is %s", getpid(), getuid());
+    }
+
+
+}
